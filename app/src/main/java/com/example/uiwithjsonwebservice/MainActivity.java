439468a622f6d024e904165657971173ebc7e640
Cristian <cristian_uteq@hotmail.com>
2020-06-10 18:43:43 -0500

    Parsing JSON API REST response with header parameters in HttpRequest

--- app/src/main/java/com/example/uiwithjsonwebservice/MainActivity.java
+++ app/src/main/java/com/example/uiwithjsonwebservice/MainActivity.java
@@ -6,13 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.uiwithjsonwebservice.WebService.Asynchtask;
+import com.example.uiwithjsonwebservice.WebService.WebService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://api-uat.kushkipagos.com/transfer-subscriptions/v1/bankList",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET","Public-Merchant-Id","84e1d0de1fbf437e9779fd6a52a9ca18");
     }
 
 
@@ -35,4 +51,17 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtBancos = (TextView)findViewById(R.id.txtListaBancos);
+        
+        String lstBancos="";
+        JSONArray JSONlista =  new JSONArray(result);
+        for(int i=0; i< JSONlista.length();i++){
+            JSONObject banco=  JSONlista.getJSONObject(i);
+            lstBancos = lstBancos + "\n" + banco.getString("name").toString();
+        }
+
+        txtBancos.setText("Respuesta WS Lista de Bancos" +  lstBancos);
     }
+}
